323ad38d01780c880f27d1d34ae4b58952e068ed
ufoo68 <mitarou.yuutarou@gmail.com>
2024-04-23 00:06:31 +0900

    use biome

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,8 +1,8 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import {
-  getServerSession,
   type DefaultSession,
   type NextAuthOptions,
+  getServerSession,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
 import DiscordProvider from "next-auth/providers/discord";
