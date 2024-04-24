6c8c063b76b1ef7071fdca8af640748a813c079b
ufoo68 <mitarou.yuutarou@gmail.com>
2024-04-24 22:20:31 +0900

    fix friend list

---
+++ src/app/friends/_components/friend-list.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import {
+  Avatar,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Input,
+  List,
+  ListItem,
+} from '@yamada-ui/react'
+import { api } from '~/trpc/react'
+
+export function FriendList() {
+  const frineds = api.friend.list.useQuery()
+  return (
+    <Card>
+      <CardHeader className="flex flex-col">
+        <Input
+          className="max-w-sm flex-1 w-full"
+          placeholder="Search"
+          type="search"
+        />
+      </CardHeader>
+      <CardBody className="grid gap-4 pt-4">
+        <List>
+          {frineds.data?.map((friend) => (
+            <ListItem className="flex items-center space-x-4 w-full">
+              <Avatar
+                className="w-10 h-10"
+                src={friend.thmbnailUrl ?? ''}
+                name={friend.name}
+              ></Avatar>
+              <div className="flex-1">
+                <div className="font-medium">{friend.name}</div>
+              </div>
+            </ListItem>
+          ))}
+        </List>
+      </CardBody>
+    </Card>
+  )
+}
--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from 'next/navigation'
 import Link from 'next/link'
 
 import { getServerAuthSession } from '~/server/auth'
-import { api } from '~/trpc/server'
+import { FriendList } from './_components/friend-list'
 import { CreateFriend } from '~/app/friends/_components/create-friend'
 export default async function Friends() {
   noStore()
@@ -32,51 +32,5 @@ export default async function Friends() {
         <FriendList />
       </div>
     </main>
-  )
-}
-
-async function FriendList() {
-  const friends = await api.friend.list.query()
-  return (
-    <div className="relative overflow-x-auto">
-      <table className="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
-        <tbody>
-          {friends.map((friend) => (
-            <tr
-              key={friend.id}
-              className="border-b bg-white dark:border-gray-700 dark:bg-gray-800"
-            >
-              <th
-                scope="row"
-                className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
-              >
-                {friend.thmbnailUrl ? (
-                  // eslint-disable-next-line @next/next/no-img-element
-                  <img
-                    className="h-12 w-12 rounded-full"
-                    src={friend.thmbnailUrl}
-                    alt="Rounded avatar"
-                  />
-                ) : (
-                  <svg
-                    className="h-12 w-12 text-gray-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                )}
-              </th>
-              <td className="text-2xl font-extrabold">{friend.name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
   )
 }
