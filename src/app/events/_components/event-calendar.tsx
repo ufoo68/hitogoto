e7b4d9677fe527f73aa6f7e976c130ff4a1ca44c
ufoo68 <mitarou.yuutarou@gmail.com>
2024-05-04 19:57:48 +0900

    微修正

--- src/app/events/_components/event-calendar.tsx
+++ src/app/events/_components/event-calendar.tsx
@@ -105,7 +105,7 @@ export function EventCalendarDynamic() {
                 </Center>
               </Indicator>
               <EventListModal
-                isOpen={dayjs(selectedDate).isSame(date, 'date')}
+                isOpen={Boolean(selectedDate) && dayjs(selectedDate).isSame(date, 'date')}
                 onClose={() => setSelectedDate(null)}
                 events={eventOnDay}
               />
